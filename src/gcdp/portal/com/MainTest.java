2de8e45e89eb0e044375dd9cc0d5779cae4d7974
Ramesh Tulaila <ramesh.tulaila@canada.ca>
2020-06-10 18:17:32 -0400

    First Commit

---
+++ src/gcdp/portal/com/MainTest.java
@@ -0,0 +1,15 @@
+package gcdp.portal.com;
+
+public class MainTest {
+
+	    public static void main(String[] args) {
+		PreScreening preScreening = new PreScreening();
+		preScreening.chooseProgram("AgriInnovate Program", "RAM Organization"); 
+		}
+	    
+	      /* public static void main(String[] args) {
+	   FullScreening fullScreening = new FullScreening();
+	   fullScreening.chooseProgram("AgriScience Program - Projects", "RAM Organization");
+	   } */
+
+  }
